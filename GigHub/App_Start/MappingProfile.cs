e305ad66a16d9d6af1e2954d59d37538eb3fa61b
f-molina <00300917@uca.edu.sv>
2020-04-18 01:02:19 -0600

    Implement notifications api

---
+++ GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using GigHub.Dtos;
+using GigHub.Models;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public static IMapper mapper;
+        public static MapperConfiguration MapperConfiguration;
+
+        public static void RegisterMappings()
+        {
+            MapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ApplicationUser, UserDto>();
+                cfg.CreateMap<Genre, GenreDto>();
+                cfg.CreateMap<Gig, GigDto>();
+                cfg.CreateMap<Notification, NotificationDto>();
+            });
+
+            mapper = MapperConfiguration.CreateMapper();
+        }
+    }
+}
--- GigHub/Global.asax.cs
+++ GigHub/Global.asax.cs
@@ -1,3 +1,4 @@
+using GigHub.App_Start;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -9,6 +10,7 @@ namespace GigHub
     {
         protected void Application_Start()
         {
+            MappingProfile.RegisterMappings();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
